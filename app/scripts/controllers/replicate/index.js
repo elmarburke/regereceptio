b8e2e279aa7547b50f2ac082deb3536577f5e86f
Elmar Burke <hallo@elmarburke.de>
2013-07-31 00:12:24 +0200

    first GH-Pages

--- app/scripts/controllers/replicate/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('regereceptioApp')
-  .controller('ReplicateIndexCtrl', function ($scope, replicator) {
-    $scope.replicateTo = function (remoteDb, newReplicate) {
-      replicator.addSync(remoteDb, newReplicate);
-    };
-  });
--- app/scripts/services/pouch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('regereceptioApp')
-  .service('pouch', function pouch(pouchClient) {
-    new PouchDB('regerecepito');
-    return pouchClient.create('regerecepito');
-  });
