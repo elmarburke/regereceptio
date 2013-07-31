4fe64f153c4b7749b24b7e4c61cd6d28c8173cea
Elmar Burke <hallo@elmarburke.de>
2013-07-31 14:57:19 +0200

    Added: Reset Database

--- app/scripts/controllers/replicate/index.js
+++ app/scripts/controllers/replicate/index.js
@@ -1,8 +1,15 @@
 'use strict';
 
 angular.module('regereceptioApp')
-  .controller('ReplicateIndexCtrl', function ($scope, replicator) {
+  .controller('ReplicateIndexCtrl', function ($scope, replicator, pouchClient, $window) {
     $scope.replicateTo = function (remoteDb, newReplicate) {
       replicator.addSync(remoteDb, newReplicate);
     };
+    $scope.resetDatabase = function () {
+        pouchClient.destroy('regereceptio')
+        .then(function(data) {
+            $window.alert("Reload!");
+            $window.location.href = '/replicate'
+        });
+    }
   });
